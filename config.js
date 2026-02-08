991e6ef34653a9143756b5d8906e62593b057e33
VENDROZ-TEC ACC-5 <mikaelkabore238@gmail.com>
2026-02-08 12:25:17 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "vdz~FrJkmLyS#htzP6Otg6yjiZ4WwN-Eh5g-wtXltrnqlQPw77TIoi2Y",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
